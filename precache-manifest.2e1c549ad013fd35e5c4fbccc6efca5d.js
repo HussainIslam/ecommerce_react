2bc4a065420dd308a30e76315d64e65de2485540
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2019-11-18 17:18:18 -0500

    Updates

---
+++ precache-manifest.2e1c549ad013fd35e5c4fbccc6efca5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cb054660d4144c22872b4d85c0e248a",
+    "url": "/ecommerce_react/index.html"
+  },
+  {
+    "revision": "e4cb530ec9178b95ea10",
+    "url": "/ecommerce_react/static/css/main.ec04031d.chunk.css"
+  },
+  {
+    "revision": "6f9bb3748f0f6b71fe80",
+    "url": "/ecommerce_react/static/js/2.c76eec0f.chunk.js"
+  },
+  {
+    "revision": "e4cb530ec9178b95ea10",
+    "url": "/ecommerce_react/static/js/main.5f699884.chunk.js"
+  },
+  {
+    "revision": "acb70db2768d5e1e2797",
+    "url": "/ecommerce_react/static/js/runtime-main.f6c02510.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/ecommerce_react/static/media/crown.ed28accb.svg"
+  }
+]);
