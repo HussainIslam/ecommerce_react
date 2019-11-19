7b60408d48041d9d85a8dba216e664d72f779702
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2019-11-19 09:13:31 -0500

    add: storing user data from signin with google

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,29 @@ const config = {
     measurementId: "G-J957F287Z6"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+    const userRef = firestore.doc(`users/${ userAuth.uid}`);
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists){
+        const { displayName, email } = userAuth;
+        const createAt = new Date();
+        try{
+            await userRef.set({
+                displayName,
+                email,
+                createAt,
+                ...additionalData
+            })
+
+        }catch (error){
+            console.log('Error creating user', error.message);
+        }
+    }
+    return userRef;
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
