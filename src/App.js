513f675558a861b660fa54d0893e2985273834bf
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2019-11-19 21:35:42 -0500

    sign-in and sign-up fully functional

--- src/App.js
+++ src/App.js
@@ -28,11 +28,11 @@ class App extends React.Component {
               id: snapShot.id,
               ...snapShot.data()
             }
-          }, () => console.log( this.state ))
+          })
         });
         
       } else {
-        this.setState({ currentUser: userAuth }, () => console.log( this.state))
+        this.setState({ currentUser: userAuth })
       }
     })
   }
